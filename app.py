1f2b1c3347c06d615501488ce1ed8063d8e0c79b
Suryanshdev1 <devsuryansh2004@gmail.com>
2026-04-28 01:09:22 +0530

    Add sys and os imports to app.py
    For deployment

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify
 import json, os, sys, random
 from datetime import datetime
